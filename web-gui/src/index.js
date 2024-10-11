12f6b4b582d7dc1ed00a0b8b1a42e039e8d5de39
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-10-11 14:56:51 +0300

    updating

--- web-gui/src/index.js
+++ web-gui/src/index.js
@@ -17,7 +17,7 @@ setRangeUpdater(range2 , (value)=>publish("/rotation_speed", "std_msgs/Float32" 
 const ArrowUp = document.getElementById("arrow-up");
 const rotateLeft = document.getElementById("rotate-left");
 const rotateRight = document.getElementById("rotate-right");
-const keys = [{ key: "ArrowUp", object: ArrowUp }, { key: "ArrowLeft", object: rotateLeft }, { key: "ArrowRight", object: rotateRight }];
+const keys = [{ key: "ArrowUp", object: ArrowUp , message : 1 }, { key: "ArrowLeft", object: rotateLeft , message:3 }, { key: "ArrowRight", object: rotateRight , message:2 }];
 const ctas = [
     { operation: "grip", object: document.getElementById("grip"),  }, 
     // line-follow - > stop
@@ -37,7 +37,7 @@ document.addEventListener("keydown", function (e) {
             if (key.key === chosenOperation) return;
             chosenOperation = key.key
             console.log(key.key);
-            publish("/keyboard_input", "std_msgs/String" , key.key);
+            publish("/keyboard_input", "std_msgs/Int32" , key.message);
         }
     }
 });
@@ -47,9 +47,9 @@ document.addEventListener("keyup", function (e) {
         if (e.key === key.key) {
             e.preventDefault();
             key.object.classList.remove("!opacity-100");
-            console.log(key.key, "up");
+            // console.log(key.key, "up");
             chosenOperation = "stop";
-            publish("/keyboard_input", "std_msgs/String" , "stop");
+            publish("/keyboard_input", "std_msgs/Int32" , 4);
         }
     }
 });
